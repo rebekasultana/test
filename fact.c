4efbc356a7271ed2de78a46b95615eb3afe802a1
rebekasultana <rebekasultana5454@gmail.com>
2019-07-17 11:32:39 +0600

    fact

---
+++ fact.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n){
+    if(n==0)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
+int main(){
+    int n, x;
+    while(1){
+    scanf("%d", &n);
+
+    x = factorial(n);
+
+    printf("the factorial of %d is: %d\n", n, x);
+}
+    return 0;
+}
+
